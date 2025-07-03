eebbc12b7b198dd9b1a4e1198f06a14496639a70
Pedro Vinicius de Castro <pedrovancastro@gmail.com>
2025-07-03 00:52:27 -0300

    chore: Remove hook nao utilizado (use-latest-song)

--- hooks/use-latest-song.ts
+++
@@ -1,9 +0,0 @@
-import fetcher from "@/lib/utils";
-import useSWR from "swr";
-import type { Response } from "../app/api/lastfm/latest/get-latest-song";
-
-export function useLatestSong(): Partial<Response> {
-  const { data } = useSWR<Response>("/api/lastfm/latest", fetcher);
-
-  return data ?? {};
-}
